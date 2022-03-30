ff44f266fa1988976b34101f802890424284b35a
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-03-30 18:49:26 +0100

    added CRUD in the database and install functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -36,39 +36,4 @@ export const getDb = async () => {
   console.log("Found results from database", result);
 };
 
-// ---------------------------------------------------------------
-
-
-
-
-
-
-// -------------------------------------------------------------------
-
-export const getAllDb = async () => {
-  console.log('GET all from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const todosDb = await openDB('todos', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('todos', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('todos');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-   // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
-};
-
-
-
-
-
-
 initdb();
